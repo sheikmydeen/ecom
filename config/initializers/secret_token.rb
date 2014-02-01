ccc029305ba83fe4d104addc92ecfbc333c15b32
sheikmydeen <sheikmydeen.10@gmail.com>
2014-02-01 07:57:10 +0530

    ROR initial Commit with changes in view.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myecom::Application.config.secret_token = '1fa0c4e5a01095f549ef30a108f2c745cb7ba8b4b0ff116b49e63b4a39926f05e4b8244c7a48ae243be4356c622c031700a94beab00467451734f0a30791914d'
